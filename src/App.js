202076612984f1a6519e607a074c47a013b85bae
ArthurJeannot <arthur.jeannot18@gmail.com>
2022-05-16 16:23:49 +0200

    Add color img + Error page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import List from './pages/List.tsx';
-import Home from './pages/Home.tsx';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import List from "./pages/List.tsx";
+import Home from "./pages/Home.tsx";
+import Error from "./pages/Error.tsx";
 
 const App = () => {
-
   return (
     <div>
       <BrowserRouter>
-          <Routes>
-          <Route path='/' element={<Home />} />
-            <Route path='/list' element={<List />} />
-            <Route path='*' element={<Home />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/list" element={<List />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ src/pages/Error.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <h1>Error 404: Page not found</h1>
+      <p>
+        <a href="/">Revenir a la page principal</a>
+      </p>
+    </div>
+  );
+};
+
+export default Error;
